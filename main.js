c45a2d2c1cec4f4b28a49afa3f3a661e9c588408
ohs710 <ohs710@newschool.edu>
2022-09-12 21:44:01 -0400

    updates
    updates index css html

---
+++ main.js
@@ -0,0 +1,8 @@
+
+const box = document.getElementsByClassName('vertical-line');
+    box.addEventListener('mouseover',function handleMouseOver(){
+        box.style.color= 'red';
+    });
+box.addEventListener('mouseout',function handleMouseOut(){
+    box.style.color='black';
+});
